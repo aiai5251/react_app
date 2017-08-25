835245a12c436c4867081e2f4a67f5c1e83abc7c
aiai5251 <didi@bogon.didichuxing.com>
2017-08-25 17:21:48 +0800

    请求

--- src/index.js
+++ src/index.js
@@ -130,6 +130,44 @@ var UserGist = React.createClass({
     }
 });
 
+var RepoList = React.createClass({
+    getInitialState: function() {
+        return {
+            loading: true,
+            error: null,
+            data: null
+        };
+    },
+    componentDidMount: function() {
+        this.props.promise.then(
+            value => this.setState({loading: false, data: value}),
+            error => this.setState({loading: false, error: error})
+        );
+    },
+    render: function() {
+        if (this.state.loading) {
+            return <span>Loading...</span>;
+        } else if (this.state.error !== null) {
+            return <span>Error: {this.state.error.message}</span>;
+        } else {
+            var repos = this.state.data.items;
+            var repoList = repos.map(function(repo) {
+                return (
+                    <li key = {repo.id}>
+                        <a href = {repo.html_url}>{repo.name}</a> ({repo.stargazers_count} stars) <br/> {repo.description}
+                    </li>
+                );
+            });
+            return (
+                <main>
+                    <h1>Most Popular JavaScript Projects in Github</h1>
+                    <ol>{repoList}</ol>
+                </main>
+            );
+        }
+    }
+});
+
 ReactDOM.render(
     <div>
         <h1>Hello, world !</h1>,
@@ -150,6 +188,9 @@ ReactDOM.render(
         <LikeButton />
         <Hello name = "world" />
         <UserGist source="https://api.github.com/users/octocat/gists" />
+        <RepoList
+            promise = {$.getJSON('https://api.github.com/search/repositories?q=javascript&sort=stars')}
+        />
     </div>,
     document.getElementById('root')
 );
